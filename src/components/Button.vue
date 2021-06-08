b9a7086802e88f785e7b5093c799ee8eeefaf312
jiayouzhao <jiayouzhaodaye@163.com>
2021-06-09 01:51:38 +0800

    Button提取组件

---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+    <span class="createBtn">
+        {{buttonName}}
+    </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+    props:{
+        buttonName:String
+    }
+})
+export default class Button extends Vue{
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+.createBtn{
+       
+    background:#767676;
+    padding:15px 20px;
+    border-radius:8px;
+    color:#fff;
+}
+</style>
