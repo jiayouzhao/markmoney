29661caee452c834cf3db6b08ed8c8649c6f85cf
jiayouzhao <jiayouzhaodaye@163.com>
2021-06-11 18:56:01 +0800

    deploy

---
+++ precache-manifest.62d487bacddbc6e996465f65e547eb1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f34d472bab112259d6",
+    "url": "/markmoney/css/app.7ae0b0a7.css"
+  },
+  {
+    "revision": "25486fd0b161224f4e82a0549a6fd3f2",
+    "url": "/markmoney/index.html"
+  },
+  {
+    "revision": "33f34d472bab112259d6",
+    "url": "/markmoney/js/app.9dccc66f.js"
+  },
+  {
+    "revision": "e7ea1b51498ca7c54b0b",
+    "url": "/markmoney/js/chunk-vendors.728bdf68.js"
+  },
+  {
+    "revision": "1e0e1af34bbc4494c22f6dad8591c23b",
+    "url": "/markmoney/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/markmoney/robots.txt"
+  }
+]);
